1d569302971e2fb08853ada7b9683b8828370920
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2024-12-23 18:18:36 +0500

    minor changes

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -5,11 +5,11 @@
 
 @section('page')
     <section>
-        <div class="container d-flex flex-column align-items-center mt-5 pt-3">
+        <div class="container d-flex flex-column align-items-center mt-2 mt-md-5 pt-3">
             <h1 class="res-title">Art-Express</h1>
             <h3 class="res-sub text-center">Unleash Your Creativity</h3>
             <p class="res-sub text-center w-75">Dive into a world of breathtaking art and inspiring masterpieces. Explore and connect with creativity like never before.</p>
-            <center><button class="btn btn-primary res-button-font">Get Started</button></center>
+            <center><button class="btn btn-primary res-button-font px-4">Get Started</button></center>
         </div>
         <div class="container-fluid mt-2 mt-md-5 pt-3 pt-md-5" id="features">
             <h1 class="text-center res-heading">Features we Provide</h1>
