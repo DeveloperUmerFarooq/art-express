bd01883b516af74c3b6c642768f321d3d33bc492
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2024-12-24 17:25:35 +0500

    minor changes

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -67,7 +67,7 @@
                 <button class="fs-1 d-block d-lg-none shadow" id="next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff0dc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg></button>
             </div>
         </div>
-        <div class="container-fluid mt-2 mt-md-5" id="art-grid" style="height: 50rem">
+        <div class="container-fluid mt-2 mt-md-5" id="art-grid">
             <h1 class="text-center res-heading" id="art-grid-title">Explore Masterpieces</h1>
         </div>
     </section>
