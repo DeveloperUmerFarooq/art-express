b3b3e4c0479de3b6eabdfdcfa016d3864403b5f5
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2024-12-27 17:37:58 +0500

    Edit functionality done

--- app/Http/Controllers/UserCrud.php
+++ app/Http/Controllers/UserCrud.php
@@ -6,6 +6,7 @@ use App\DataTables\ArtistDataTable;
 use App\DataTables\UsersDataTable;
 use App\Models\User;
 use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -19,21 +20,36 @@ class UserCrud extends Controller
             return $datatable->render('admin.user-management.artist');
     }
     public function delete($id){
-        User::find($id)->delete();
-        toastr()->success('User Deleted!');
+        try{
+            User::find($id)->delete();
+            toastr()->success('User Deleted!');
+        }
+        catch(Exception $error){
+            toastr()->error('Operation Failed');
+        }
         return redirect()->back();
     }
     public function addArtist(Request $req){
-        $user=User::create([
-            'name'=>$req->name,
-            'email'=>$req->email,
-            'password'=>Hash::make($req->password),
+        $req->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:users,email',
+            'password'=>'required|min:8|confirmed'
         ]);
-        $user->assignRole('artist');
-        $user->profile()->create([
-            'profile_image' => 'https://ui-avatars.com/api/?name=' . urlencode($user->name),
-        ]);
-        toastr()->success('New Artist Created');
+        try{
+            $user=User::create([
+                'name'=>$req->name,
+                'email'=>$req->email,
+                'password'=>Hash::make($req->password),
+            ]);
+            $user->assignRole('artist');
+            $user->profile()->create([
+                'profile_image' => 'https://ui-avatars.com/api/?name=' . urlencode($user->name),
+            ]);
+            toastr()->success('New Artist Created');
+        }
+        catch(Exception $error){
+            toastr()->error('Artist Creation Failed');
+        }
         return redirect()->back();
     }
     public function addUser(Request $req){
@@ -42,16 +58,21 @@ class UserCrud extends Controller
             'email'=>'required|email|unique:users,email',
             'password'=>'required|min:8|confirmed'
         ]);
-        $user=User::create([
-            'name'=>$req->name,
-            'email'=>$req->email,
-            'password'=>Hash::make($req->password),
-        ]);
-        $user->assignRole('user');
-        $user->profile()->create([
-            'profile_image' => 'https://ui-avatars.com/api/?name=' . urlencode($user->name),
-        ]);
-        toastr()->success('New User Created');
+        try{
+            $user=User::create([
+                'name'=>$req->name,
+                'email'=>$req->email,
+                'password'=>Hash::make($req->password),
+            ]);
+            $user->assignRole('user');
+            $user->profile()->create([
+                'profile_image' => 'https://ui-avatars.com/api/?name=' . urlencode($user->name),
+            ]);
+            toastr()->success('New User Created');
+        }
+        catch(Exception $error){
+            toastr()->error("User Creation Failed!");
+        }
         return redirect()->back();
     }
     public function editUsers(Request $req){
