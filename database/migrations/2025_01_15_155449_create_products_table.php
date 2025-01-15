b30ec6ba843a8a8f0ac52efa2c69284cad312555
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-01-15 21:44:50 +0500

    Added new Migrations

---
+++ database/migrations/2025_01_15_155449_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('type', ['Portrait', 'Landscape']);
+            $table->foreignId('blog_id')->constrained('blogs')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('image_id')->constrained('images')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('artist_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('category_id')->constrained('sub_categories')->onDelete('cascade')->onUpdate('cascade');
+            $table->text('description');
+            $table->decimal('price',10,2);
+            $table->json('materials')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
