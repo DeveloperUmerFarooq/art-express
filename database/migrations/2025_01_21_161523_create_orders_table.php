d9fb01fff806d6332d595095313d1a1d28f31e08
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-02-07 17:08:54 +0500

    Updated Orders Query

--- database/migrations/2025_01_21_161523_create_orders_table.php
+++ database/migrations/2025_01_21_161523_create_orders_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('customer_name');
             $table->date('order_date');
             $table->enum('status', ['Processing', 'Completed'])->default('Processing');
             $table->string('product_name');
