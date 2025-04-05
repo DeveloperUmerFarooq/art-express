57fe411d4d0b91c0a968d57ef6221928de248921
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-04-05 18:16:06 +0500

    updated landing layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,78 +5,84 @@
 
 @section('page')
     <section>
-        <div class="container d-flex flex-column align-items-center mt-2 mt-md-3 pt-3">
-            <center><img loading="lazy" src="{{asset('assets/images/logo.svg')}}" id="logo"
-                 height="150" alt="" style="filter:drop-shadow(1px 1px 10px #131010)"></center>
-            <h1 class="res-title">Art-Express</h1>
-            <h3 class="res-sub text-center">Get the Art you Desire</h3>
-            <p class="res-sub text-center w-75">Dive into a world of breathtaking art and inspiring masterpieces. Explore and connect with creativity like never before.</p>
-            @if (auth()->user())
-            <center>
-                <a href="{{route($role.'.dashboard')}}">
-                    <button class="btn btn-primary res-button-font px-4" id="hero-button">Get Started</button>
-                </a>
-            </center>
-            @else
-            <center>
-                <button class="btn btn-primary res-button-font px-4" id="hero-button" data-bs-toggle="modal" data-bs-target="#Sign-Up">Get Started</button>
-            </center>
-            @endif
+        <div class="py-2 py-md-3" id="hero-section">
+            <div class="container d-flex flex-column align-items-center py-3 glasmorphism">
+                <center><img loading="lazy" src="{{asset('assets/images/logo.svg')}}" id="logo"
+                     height="150" alt="" style="filter:drop-shadow(1px 1px 10px #131010)"></center>
+                <h1 class="res-title">Art-Express</h1>
+                <h3 class="res-sub text-center">Get the Art you Desire</h3>
+                <p class="res-sub text-center w-75">Dive into a world of breathtaking art and inspiring masterpieces. Explore and connect with creativity like never before.</p>
+                @if (auth()->user())
+                <center>
+                    <a href="{{route($role.'.dashboard')}}">
+                        <button class="btn btn-primary res-button-font px-4" id="hero-button">Get Started</button>
+                    </a>
+                </center>
+                @else
+                <center>
+                    <button class="btn btn-primary res-button-font px-4" id="hero-button" data-bs-toggle="modal" data-bs-target="#Sign-Up">Get Started</button>
+                </center>
+                @endif
+            </div>
         </div>
-        <div class="container-fluid mt-2 mt-md-5 pb-3 pt-3 pb-md-5 pt-md-5 position-relative" id="features">
-            <h1 class="text-center res-heading">Features we Provide</h1>
-            <div class="container-fluid px-md-3 mt-md-5 mt-3">
-                <div class="owl-carousel py-md-2 py-md-3">
-                    <div class="slider-card pb-md-2 d-flex align-items-center justify-content-center">
-                        <div class="card features-card" >
-                        <img loading="lazy" src="{{asset('assets/images/images.jpeg')}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">Discover Art Pieces</h5>
-                          <p class="card-text">Explore an exclusive collection of art pieces from talented artists around the globe. Find the perfect artwork to match your style and personality.</p>
+        <div id="features">
+            <div class="container-fluid pb-3 pb-md-5 position-relative">
+                <div class="pt-md-5 pt-3">
+                    <h1 class="text-center res-heading">Features we Provide</h1>
+                    <div class="container-fluid px-md-3 mt-md-5 mt-3">
+                        <div class="owl-carousel py-md-2 py-md-3">
+                            <div class="slider-card pb-md-2 d-flex align-items-center justify-content-center">
+                                <div class="card features-card" >
+                                <img loading="lazy" src="{{asset('assets/images/images.jpeg')}}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <h5 class="card-title">Discover Art Pieces</h5>
+                                  <p class="card-text">Explore an exclusive collection of art pieces from talented artists around the globe. Find the perfect artwork to match your style and personality.</p>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="slider-card text-white">
+                                <div class="card features-card" >
+                                    <img loading="lazy" src="{{asset('assets/images/Artist Showcase.webp')}}" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Artist Showcase</h5>
+                                      <p class="card-text">Celebrate the creativity of emerging and renowned artists. Browse through curated profiles and learn the stories behind their masterpieces.</p>
+                                    </div>
+                                  </div>
+                            </div>
+                            <div class="slider-card text-white">
+                                <div class="card features-card" >
+                                    <img loading="lazy" src="{{asset('assets/images/shutterstock_451991974.jpg')}}" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Blog for Art Enthusiasts</h5>
+                                      <p class="card-text">Dive into engaging blogs about art trends, techniques, and history. Stay inspired and connected with the art community.</p>
+                                    </div>
+                                  </div>
+                            </div>
+                            <div class="slider-card text-white">
+                                <div class="card features-card" >
+                                    <img loading="lazy" src="{{asset('assets/images/chatGPT.jpg.webp')}}" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">ChatGPT- Assistance</h5>
+                                      <p class="card-text">Need help choosing art? Use our AI-powered chat tool to learn more about art categories make your selection easier.</p>
+                                    </div>
+                                  </div>
+                            </div>
+                            <div class="slider-card">
+                                <div class="card features-card">
+                                    <img loading="lazy" src="{{asset('assets/images/online-shopping-2.jpg')}}" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Secure Online Shopping</h5>
+                                      <p class="card-text">Shop for art worry-free with our secure payment system. Enjoy a seamless checkout experience and reliable delivery services.</p>
+                                    </div>
+                                  </div>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                    <div class="slider-card text-white">
-                        <div class="card features-card" >
-                            <img loading="lazy" src="{{asset('assets/images/Artist Showcase.webp')}}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                              <h5 class="card-title">Artist Showcase</h5>
-                              <p class="card-text">Celebrate the creativity of emerging and renowned artists. Browse through curated profiles and learn the stories behind their masterpieces.</p>
-                            </div>
-                          </div>
-                    </div>
-                    <div class="slider-card text-white">
-                        <div class="card features-card" >
-                            <img loading="lazy" src="{{asset('assets/images/shutterstock_451991974.jpg')}}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                              <h5 class="card-title">Blog for Art Enthusiasts</h5>
-                              <p class="card-text">Dive into engaging blogs about art trends, techniques, and history. Stay inspired and connected with the art community.</p>
-                            </div>
-                          </div>
-                    </div>
-                    <div class="slider-card text-white">
-                        <div class="card features-card" >
-                            <img loading="lazy" src="{{asset('assets/images/chatGPT.jpg.webp')}}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                              <h5 class="card-title">ChatGPT- Assistance</h5>
-                              <p class="card-text">Need help choosing art? Use our AI-powered chat tool to learn more about art categories make your selection easier.</p>
-                            </div>
-                          </div>
-                    </div>
-                    <div class="slider-card">
-                        <div class="card features-card">
-                            <img loading="lazy" src="{{asset('assets/images/online-shopping-2.jpg')}}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                              <h5 class="card-title">Secure Online Shopping</h5>
-                              <p class="card-text">Shop for art worry-free with our secure payment system. Enjoy a seamless checkout experience and reliable delivery services.</p>
-                            </div>
-                          </div>
+                    <div class="d-flex gap-3 justify-content-center ">
+                        <button class="fs-1 d-block d-lg-none shadow" id="prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff0dc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg></button>
+                        <button class="fs-1 d-block d-lg-none shadow" id="next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff0dc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg></button>
                     </div>
                 </div>
-            </div>
-            <div class="d-flex gap-3 justify-content-center ">
-                <button class="fs-1 d-block d-lg-none shadow" id="prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff0dc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg></button>
-                <button class="fs-1 d-block d-lg-none shadow" id="next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff0dc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg></button>
             </div>
         </div>
         <div class="container-fluid mt-2 mt-md-5" id="art-grid">
