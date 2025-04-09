6b103e7a83cc6d7ec8da8600e125d129dad73c55
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-04-09 15:50:10 +0500

    minor changes

--- app/Models/Blogs.php
+++ app/Models/Blogs.php
@@ -14,7 +14,7 @@ class Blogs extends Model
         return $this->belongsTo(Products::class,'product_id');
     }
     public function comments(){
-        return $this->hasMany(Comment::class,'post_id')->orderBy('created_at', 'desc');;
+        return $this->hasMany(Comment::class,'post_id')->orderBy('created_at', 'desc');
     }
 
     public function likes(){
