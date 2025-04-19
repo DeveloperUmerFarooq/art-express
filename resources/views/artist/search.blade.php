6bfa4675b95964f5bd43a0e4d512829136a29330
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-04-19 11:24:42 +0500

    minor changes

--- resources/views/artist/search.blade.php
+++ resources/views/artist/search.blade.php
@@ -2,15 +2,23 @@
 
 @section('page')
 
-<div class="container mt-3 mb-3">
-    <input type="search" placeholder="search artist name/email..." class="form-control" id="search">
-</div>
-<h1 class="text-center">Artists</h1>
-<div class="mx-2 mx-lg-4">
-    {{ $dataTable->table() }}
+<div class="container mt-5 mb-4">
+    <!-- Search Bar Section -->
+    <div class="d-flex justify-content-center mb-4">
+        <input type="search" placeholder="Search artist name/email..." class="form-control w-50" id="search">
+    </div>
+
+    <!-- Heading Section -->
+    <h1 class="text-center text-primary mb-5">Artists</h1>
+
+    <!-- DataTable Section -->
+    <div class="mx-2 mx-lg-4">
+        {{ $dataTable->table(['class' => 'table table-striped table-hover table-bordered shadow-sm']) }}
+    </div>
 </div>
 
 @endsection
+
 @push('scripts')
 {{ $dataTable->scripts() }}
 <script>
