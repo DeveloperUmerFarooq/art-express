5cc6fa7fd3996f3cda84f18203c1ed34d0402a07
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-05-02 20:57:40 +0500

    minor changes in admin controller index function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,21 +10,30 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function index(){
-        $monthlyUsers=collect(range(1,12))->map(function($month){
-            return User::whereMonth('created_at',$month)->whereYear('created_at',now()->year)->whereHas('roles', function ($query) {
+    public function index()
+    {
+
+        $monthlyUsers = collect(range(1, 12))->map(function ($month) {
+            return User::whereMonth('created_at', $month)->whereYear('created_at', now()->year)
+            ->whereHas('roles', function ($query) {
                 $query->where('name', '!=', 'admin');
             })->count();
         })->toArray();
-        $monthlySales=collect(range(1,12))->map(function($month){
-            return Order::whereMonth('created_at',$month)->whereYear('created_at',now()->year)->with('items')->get()->flatMap->items->sum('total_price');
+
+        $monthlySales = collect(range(1, 12))->map(function ($month) {
+            return Order::whereMonth('created_at', $month)->whereYear('created_at', now()->year)->with('items')->get()->flatMap->items->sum('total_price');
         })->toArray();
-        $users=User::Role('user')->count();
-        $artists=User::Role('artist')->count();
-        $sales=Order::whereMonth('created_at',now()->month)->whereYear('created_at',now()->year)->with('items')->get()->flatMap->items->sum('total_price');
-        $totalSales=Order::whereYear('created_at',now()->year)->with('items')->get()->flatMap->items->sum('total_price');
-        $products=Products::count();
-        $blogs=Blogs::count();
-        return view('admin.dashboard',compact('sales','totalSales','users','artists','products','blogs','monthlyUsers','monthlySales'));
+
+        $users = User::Role('user')->count();
+        $artists = User::Role('artist')->count();
+
+        $sales = Order::whereMonth('created_at', now()->month)->whereYear('created_at', now()->year)->with('items')->get()->flatMap->items->sum('total_price');
+
+        $totalSales = Order::whereYear('created_at', now()->year)->with('items')->get()->flatMap->items->sum('total_price');
+
+        $products = Products::count();
+        $blogs = Blogs::count();
+
+        return view('admin.dashboard', compact('sales', 'totalSales', 'users', 'artists', 'products', 'blogs', 'monthlyUsers', 'monthlySales'));
     }
 }
