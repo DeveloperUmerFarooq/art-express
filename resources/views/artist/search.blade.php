b73b2fc19133444f8481dc7a32c5c1c0516ac726
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-05-03 15:39:24 +0500

    minor changes

--- resources/views/artist/search.blade.php
+++ resources/views/artist/search.blade.php
@@ -3,17 +3,18 @@
 @section('page')
 
 <div class="container mt-5 mb-4">
+
+    <!-- Heading Section -->
+    <h1 class="text-center mb-5">Artists</h1>
+
     <!-- Search Bar Section -->
     <div class="d-flex justify-content-center mb-4">
         <input type="search" placeholder="Search artist name/email..." class="form-control w-50" id="search">
     </div>
 
-    <!-- Heading Section -->
-    <h1 class="text-center text-primary mb-5">Artists</h1>
-
     <!-- DataTable Section -->
     <div class="mx-2 mx-lg-4">
-        {{ $dataTable->table(['class' => 'table table-striped table-hover table-bordered shadow-sm']) }}
+        {{ $dataTable->table(['class' => 'table table-striped table-hover bg-white my-3 table-bordered shadow-sm']) }}
     </div>
 </div>
 
