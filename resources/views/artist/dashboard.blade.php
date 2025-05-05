3969e7729314333999c24b02f10c36e7cdf854ce
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-05-05 15:50:15 +0500

    minor changes

--- resources/views/artist/dashboard.blade.php
+++ resources/views/artist/dashboard.blade.php
@@ -23,8 +23,8 @@ Artist Dashboard
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Total Sales</div>
+                            <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">
+                                Revenue</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{$totalMonthlySaleAmount}} Rs</div>
                         </div>
                         <div class="col-auto">
