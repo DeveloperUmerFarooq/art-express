abce6e53e5174af184e1cf4d01523bcc52fb7dcb
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-05-20 16:45:58 +0500

    Added preview modal

---
+++ resources/views/auction/modals/image-preview.blade.php
@@ -0,0 +1,10 @@
+<div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-fullscreen modal-dialog-centered">
+        <div class="modal-content bg-transparent border-0 shadow-none">
+            <button type="button" class="btn-close btn-close-white position-absolute top-0 end-0 m-4 z-3" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="modal-body d-flex justify-content-center align-items-center p-0">
+                <img loading="lazy" id="modalImage" src="" alt="Preview" class="img-fluid" style="max-height: 90vh; object-fit: contain;">
+            </div>
+        </div>
+    </div>
+</div>
