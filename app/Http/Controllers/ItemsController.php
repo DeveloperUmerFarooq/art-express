d005e6080214b3d3289baad24ec7ad0cca951037
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-05-30 17:43:42 +0500

    Added edit auction item

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -8,18 +8,19 @@ use Illuminate\Http\Request;
 class ItemsController extends Controller
 {
 
-    public function store(Request $req){
+    public function store(Request $req)
+    {
         $req->validate([
-            'name'=>'required|string',
-            'description'=>'required|string',
-            'starting_bid'=>'required|min:1',
-            'image'=>'required|mimes:jpeg,png,jpg'
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'starting_bid' => 'required|min:1',
+            'image' => 'required|mimes:jpeg,png,jpg'
         ]);
-        try{
-        if($req->hasFile('image')){
-            $image = $req['image']->store('auction_items', 'public');
-            $imagePath = asset('storage/' . $image);
-            AuctionItem::create([
+        try {
+            if ($req->hasFile('image')) {
+                $image = $req['image']->store('auction_items', 'public');
+                $imagePath = asset('storage/' . $image);
+                AuctionItem::create([
                     'auction_id' => $req->auction_id,
                     'name' => $req['name'],
                     'description' => $req['description'],
@@ -28,24 +29,49 @@ class ItemsController extends Controller
                     'winner_id' => null,
                     'image' => $imagePath,
                 ]);
-            toastr()->success("Item Added Successfully!");
-        }
-        }catch(\Exception $error){
+                toastr()->success("Item Added Successfully!");
+            }
+        } catch (\Exception $error) {
             toastr()->error("Operation Failed!");
         }
         return redirect()->back();
         dd($req->toArray());
     }
 
-    public function update(Request $req){
-        $req->toArray();
+    public function update(Request $req)
+    {
+        $req->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'starting_bid' => 'required|min:1',
+            'image' => 'mimes:jpeg,png,jpg|nullable'
+        ]);
+        try {
+            $item = AuctionItem::find($req->item_id);
+            $imagePath=$item->image;
+            if($req->hasFile('image')){
+                $image = $req['image']->store('auction_items', 'public');
+                $imagePath = asset('storage/' . $image);
+            }
+            $item->update([
+                'name' => $req['name']??$item->name,
+                'description' => $req['description']??$item->desciption,
+                'starting_bid' => $req['starting_bid']??$item->starting_bid,
+                'image' => $imagePath,
+            ]);
+            toastr()->success("Item Updated Successfully!");
+        } catch (\Exception $error) {
+            toastr()->error("Operation Failed!");
+        }
+        return redirect()->back();
     }
 
-    public function delete($id){
-        try{
-            $items=AuctionItem::find($id)->delete();
+    public function delete($id)
+    {
+        try {
+            $items = AuctionItem::find($id)->delete();
             toastr()->success("Item Deleted Successfully!");
-        }catch(\Exception $error){
+        } catch (\Exception $error) {
             toastr()->error("Operation Failed!");
         }
         return redirect()->back();
