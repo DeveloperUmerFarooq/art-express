06e5e048f52687f12c35b802c1ccc3e4fd00064f
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-10-11 22:36:55 +0500

    Refactor auction handling logic to improve clarity and maintainability

--- app/Console/Commands/HandleAuctionSchedules.php
+++ app/Console/Commands/HandleAuctionSchedules.php
@@ -18,36 +18,38 @@ class HandleAuctionSchedules extends Command
     {
         $now = Carbon::now();
 
+        // END AUCTIONS
+        $auctionsToEnd = Auction::where('status','!=','ended')->get();
+        if(!empty($auctionsToEnd)){
+            foreach ($auctionsToEnd as $auction) {
+                $auctionEnd = Carbon::parse($auction->start_date . ' ' . $auction->end_time);
+                if ($now->greaterThanOrEqualTo($auctionEnd)) {
+                    foreach ($auction->items as $item) {
+                        if ($item->winner) {
+                            Mail::to($item->winner->email)->send(new WinnerPaymentMail($item, $auction));
+                        }
+                    }
+                    $auction->update(['status' => 'ended']);
+                    $this->info("Ended auction ID: {$auction->id}");
+                }
+                broadcast(new AuctionEndEvent($auction->id));
+            }
+        }
+
         // START AUCTIONS
         $auctionsToStart = Auction::where('status', 'upcoming')->get();
-        foreach ($auctionsToStart as $auction) {
-            $auctionStart = Carbon::parse($auction->start_date . ' ' . $auction->start_time)->addMinutes(5);
-            $auctionEnd = Carbon::parse($auction->start_date . ' ' . $auction->end_time);
-            if ($now->greaterThanOrEqualTo($auctionStart) && $now->lessThan($auctionEnd)) {
-                $auction->update(['status' => 'ongoing']);
-                foreach ($auction->registeredUsers as $user) {
-                    Mail::to($user->email)->send(new AuctionStart($auction, $user));
-                }
-                $this->info("Started auction ID: {$auction->id}");
-            }
-        }
-
-        // END AUCTIONS
-        $auctionsToEnd = Auction::where('status', 'ongoing')->get();
-        foreach ($auctionsToEnd as $auction) {
-            $auctionEnd = Carbon::parse($auction->start_date . ' ' . $auction->end_time);
-            if ($now->greaterThanOrEqualTo($auctionEnd)) {
-                foreach ($auction->items as $item) {
-                    if ($item->winner) {
-                        Mail::to($item->winner->email)->send(new WinnerPaymentMail($item, $auction));
+        if(!empty($auctionsToStart)){
+            foreach ($auctionsToStart as $auction) {
+                $auctionStart = Carbon::parse($auction->start_date . ' ' . $auction->start_time)->addMinutes(5);
+                $auctionEnd = Carbon::parse($auction->start_date . ' ' . $auction->end_time);
+                if ($now->greaterThanOrEqualTo($auctionStart) && $now->lessThan($auctionEnd)) {
+                    $auction->update(['status' => 'ongoing']);
+                    foreach ($auction->registeredUsers as $user) {
+                        Mail::to($user->email)->send(new AuctionStart($auction, $user));
                     }
+                    $this->info("Started auction ID: {$auction->id}");
                 }
-                $auction->update(['status' => 'ended']);
-                $this->info("Ended auction ID: {$auction->id}");
             }
-            broadcast(new AuctionEndEvent($auction->id));
         }
-
-        return 0;
-    }
+}
 }
