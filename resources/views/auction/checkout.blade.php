67b5097d99486ba0d67aa46bb750416feb10eb5e
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-12-02 22:59:41 +0500

    minor changes

--- resources/views/auction/checkout.blade.php
+++ resources/views/auction/checkout.blade.php
@@ -5,19 +5,20 @@
     @section('page')
         <div class="py-5">
             <div class="container bg-light shadow mx-auto p-5 d-flex flex-column gap-2">
-                <div class="d-flex flex-column align-items-center">
+                <div class="d-flex flex-column align-items-center gap-4">
                     <img src="{{ $item->image }}" class="object-fit-contain w-25" alt="">
-                    <div class="info text-center">
+                    <div class="d-flex flex-column gap-2 info text-center">
                         <h1>{{ $item->name }}</h1>
-                        <h3 class="bg-success text-light w-auto d-inline p-2 rounded-4">Rs
+                        <h3 class="bg-success text-light w-auto d-inline p-2  rounded-4">Rs
                             {{ number_format($item->current_bid, 0) }}</h3>
                     </div>
                 </div>
-                <div class="d-flex justify-content-center">
+                <div class="d-flex justify-content-center mt-3">
                     <div class="text-center w-50">
                         <label class="form-label fw-bolder fs-2">Card Details</label>
                         <div id="card-element" class="form-control py-3 mb-3"></div>
-                        <button class="btn btn-success btn-lg" onclick="generateToken()">Pay Now</button>
+                        <button class="btn btn-success btn-lg py-2" id="pay-button" onclick="generateToken()">Pay Now <div
+                                class="spinner-grow spinner-grow-sm d-none" id="loader" role="status"></div></button>
                     </div>
                 </div>
                 <form action="{{ route('item.checkout') }}" method="POST" id="payment-form">
@@ -33,7 +34,10 @@
             var elements = stripe.elements();
             var cardElement = elements.create('card');
             cardElement.mount("#card-element");
+
             function generateToken() {
+                $("#loader").removeClass("d-none");
+                $("#pay-button").attr("disabled", true);
                 stripe.createToken(cardElement).then(function(result) {
                     if (result.token) {
                         $('#stripe-token').val(result.token.id);
